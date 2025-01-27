f68a5b99a262914a39e9e9bbd3e06d20cbd01ac0
BurcuMengu <burcumengu@gmail.com>
2025-01-27 18:29:52 +0300

    push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import TabButton from "./components/TabButton"
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section id="examples">
+      <h2>Examples</h2>
+        <TabButton onSelect={() => handleSelect("Todays's Deals")}>Todays's Deals</TabButton>
+        <TabButton onSelect={() => handleSelect("Buy Again")}>Buy Again</TabButton>
+        <TabButton onSelect={() => handleSelect("Customer Service")}>Customer Service</TabButton>
+        <TabButton onSelect={() => handleSelect("Gift Cards")}>Gift Cards</TabButton>
+    </section>
   )
 }
 
