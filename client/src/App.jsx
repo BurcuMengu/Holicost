eca8cc8719a26e349680888051d2d8b36235edd7
BurcuMengu <burcumengu@gmail.com>
2025-01-27 20:07:23 +0300

    push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,23 @@ import { useState } from 'react'
 import TabButton from "./components/TabButton"
 
 function App() {
+  const [ selectedTopic, setSelectedTopic] = useState("Please click a button");
+
   function handleSelect(selectedButton) {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
   }
 
   return (
     <section id="examples">
       <h2>Examples</h2>
-        <TabButton onSelect={() => handleSelect("Todays's Deals")}>Todays's Deals</TabButton>
-        <TabButton onSelect={() => handleSelect("Buy Again")}>Buy Again</TabButton>
-        <TabButton onSelect={() => handleSelect("Customer Service")}>Customer Service</TabButton>
-        <TabButton onSelect={() => handleSelect("Gift Cards")}>Gift Cards</TabButton>
+      <menu>
+          <TabButton onSelect={() => handleSelect("Todays's Deals")}>Todays's Deals</TabButton>
+          <TabButton onSelect={() => handleSelect("Buy Again")}>Buy Again</TabButton>
+          <TabButton onSelect={() => handleSelect("Customer Service")}>Customer Service</TabButton>
+          <TabButton onSelect={() => handleSelect("Gift Cards")}>Gift Cards</TabButton>
+        </menu>
+        {selectedTopic}
     </section>
   )
 }
